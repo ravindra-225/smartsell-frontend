6b7bef5ec5150511c86c5a89f5413b8d84fd11af
Ravindra <ravindrakaranam95@gmail.com>
2025-08-17 19:25:27 +0530

    changes in login to display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { Login } from './login/login';
 import { ProductList } from './product-list/product-list';
 import { AddProduct } from './add-product/add-product';
 import { AuthGuard } from './auth-guard';
-
-export const routes: Routes = [
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+ const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: Login },
@@ -13,3 +14,8 @@ export const routes: Routes = [
     { path: 'add-product', component: AddProduct, canActivate: [AuthGuard] },
 ];
 
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule, CommonModule]
+})
+export class AppRoutingModule {}
